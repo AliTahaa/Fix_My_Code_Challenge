f5b68985ad44fea13285fe62e6c72f4f8eedc5c3
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-16 07:50:47 +0300

    fix 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """ Module for square class"""
 
@@ -17,7 +18,7 @@ class Square():
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        """ Perimeter of the square """
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
